523c1b1b9456c71510c17a723bd01add770a163c
meeh16 <manushree1604@gmail.com>
2026-07-10 09:20:21 +0530

    Update test endpoint to print environment variables

--- api/test.ts
+++ api/test.ts
@@ -1,5 +1,10 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({ status: "ok", message: "minimal test function works" });
+  res.status(200).json({ 
+    status: "ok", 
+    VERCEL: process.env.VERCEL, 
+    NODE_ENV: process.env.NODE_ENV,
+    PORT: process.env.PORT 
+  });
 }
